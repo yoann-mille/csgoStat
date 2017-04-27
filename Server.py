7d16154acca2ba57e5d0d3fdcfd05f8ecc00ec40
yoann mille <yupindev@gmail.com>
2017-04-27 22:39:07 +0200

    Pb bombe exploded 2 frame

--- Server.py
+++ Server.py
@@ -5,11 +5,12 @@ HTTP Server to receive change stat in a cs go game
 
 from http.server import BaseHTTPRequestHandler,HTTPServer
 import config
-import json
 import stats
 
 
 class reqHandler(BaseHTTPRequestHandler):
+    parser = stats.ParserStat()
+
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -22,13 +23,9 @@ class reqHandler(BaseHTTPRequestHandler):
         self._set_headers()
 
     def do_POST(self):
-        # Doesn't do anything with posted data
         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
         post_data = self.rfile.read(content_length)
-           # self.rfile.read(content_length)  # <--- Gets the data itself
-        #print(post_data)
-        #print(str(post_data, 'utf-8'))
-        stats.ParserStat().parse_data(str(post_data, 'utf-8'))
+        self.parser.parse_data(str(post_data, 'utf-8'))
         self._set_headers()
 
 
--- stats.py
+++ stats.py
@@ -16,6 +16,8 @@ class MatchState(Enum):
     WARMUP = 'warmup'
     LIVE = 'live'
     OVER = 'gameover'
+    SWAP = 'intermission'
+
 
 class ParserStat:
     now = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d_%Hh%M')
@@ -28,88 +30,52 @@ class ParserStat:
         self.mvps = 0
         self.scores = 0
 
+    def parse_round(self):
+        player = self.frame.player
+        map = self.frame.map
+        side = self.frame.player['team'] if 'team' in self.frame.player else 0
+        print('SIDE', side)
+        t_score = map['team_t']['score']
+        ct_score = map['team_ct']['score']
+        nbr = map['round']
+        kill = player['state']['round_kills'] if 'round_kills' in player['state'] else 0
+        killhs = player['state']['round_killhs'] if 'round_killhs' in player['state'] else 0
+        assist = player['state']['round_assists'] if 'round_assists' in player['state'] else 0
+        death = player['state']['round_death'] if 'round_death' in player['state'] else 0
+        score = player['match_stats']['score'] if 'score' in player['match_stats'] else 0
+        mvp = player['match_stats']['mvps'] if 'mvps' in player['match_stats'] else 0
+        self.match.add_round(copy.copy(Round(side, t_score, ct_score, nbr, kill, killhs, assist, death, score, mvp)))
+
     def parse_data(self, post_data):
         self.frame = Frame(post_data)
-
+        if not ('previously' in self.frame.__dict__):
+            return 0
         if 'map' in self.frame.__dict__:
+            if self.state != MatchState.LIVE and self.frame.round['phase'] == MatchState.LIVE.value:
+                print('LIVE')
+                self.state = MatchState.LIVE
+                self.match.update_map(self.frame.map['name'])
+            print('[FRAME]')
+            print(self.frame.__dict__)
             if self.frame.map['phase'] == MatchState.WARMUP.value:
                 print("warmup")
                 return 0
-            elif self.frame.map['phase'] == MatchState.LIVE.value:
-                print("LIVE")
+            elif self.state == MatchState.LIVE and self.frame.map['phase'] == MatchState.LIVE.value:
                 # First round
-
-                if self.state.value != MatchState.LIVE.value:
-                    self.state = MatchState.LIVE
-                    print(self.frame.map['name'])
-                    self.match.update_map(self.frame.map['name'])
-                    #self.match.map = self.frame.map['name']
-
-                    """
-                    self.round_count = self.frame.map['round']
-                    
-                    team = 'T'
-                    if 'team' in self.frame.player:
-                        team = self.frame.player['team']
-                    self.round = Round(team,
-                                       self.frame.map['team_ct']['score'],
-                                       self.frame.map['team_t']['score'],
-                                       self.frame.map['round'])
-                    """
                 if self.frame.player['steamid'] == config.Steam['yupin_id']:
-                    print("[YUPIN]")
-                    #print(self.frame.__dict__)
-                    if self.frame.round['phase'] == 'over':
+                    if self.frame.round['phase'] == 'over' and not ('phase' in self.frame.previously['map']):
                         print("[ROUND_OVER]")
-                        print(self.frame.__dict__)
-                        player = self.frame.player
-                        map = self.frame.map
-                        side = self.frame.player['team']
-                        t_score = map['team_t']['score']
-                        ct_score = map['team_ct']['score']
-                        nbr = map['round']
-                        kill = player['state']['round_kills'] if 'round_kills' in player['state'] else 0
-                        killhs = player['state']['round_killhs'] if 'round_killhs' in player['state'] else 0
-                        assist = player['state']['round_assists'] if 'round_assists' in player['state'] else 0
-                        death = player['state']['round_death'] if 'round_death' in player['state'] else 0
-                        score = player['match_stats']['score'] if 'score' in player['match_stats'] else 0
-                        mvp = player['match_stats']['mvps'] if 'mvps' in player['match_stats'] else 0
-                        self.match.add_round(copy.copy(Round(side, t_score, ct_score, nbr, kill, killhs, assist, death, score, mvp)))
-                """       
-                 #  All round for a specific player id
-
-                    player = self.frame.player
-
-                    #   New Round
-
-                    if self.round.nbr != self.frame.map['round']:
-                        self.match.add_round(copy.copy(self.round))
-                        self.round.reset(self.frame.player['team'],
-                                       self.frame.map['team_ct']['score'],
-                                       self.frame.map['team_t']['score'],
-                                       self.frame.map['round'])
-                    else:
-
-                        #  Same round
-                        print("SAME ROUND")
-                        stats = player['match_stats']
-                        assist = stats.assists - self.round.assist
-                        death = stats.deaths - self.round.death
-                        score = stats.score - self.scores
-                        mvp = stats.mvp - self.mvps
-                        self.round.update(player['team'], player.state.round_kill, player.state.round_killhs, assist, death, score, mvp)
-                        self.mvps = stats.mvp
-                        self.scores = stats.score
-                """
-            elif self.frame.map['phase'] == MatchState.OVER.value:
-
+                        self.parse_round()
+            elif self.state == MatchState.LIVE and self.frame.map['phase'] == MatchState.SWAP.value and not ('phase' in self.frame.previously['map']):
+                print('SWAP')
+                self.parse_round()
+            elif self.state != MatchState.WARMUP and self.frame.map['phase'] == MatchState.OVER.value:
+                self.state = MatchState.WARMUP
+                self.parse_round()
                 # Export vers db
                 print("[GAME_OVER]")
-                #print("match json")
-                #print(json.dumps(self.match))
-                print("match self")
                 print(self.match.__dict__)
-                print("Match")
-                print(Match.__dict__)
+                for rnd in self.match.rounds:
+                    print(rnd.__dict__)
                 print("END")
                 print("END")
